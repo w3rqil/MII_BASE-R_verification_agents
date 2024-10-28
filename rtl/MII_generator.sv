b6a8eaf6604c4afb7603834ff1b6f0f72f604706
Ruben-Zuniga <rubenzu41@gmail.com>
2024-10-28 10:45:30 -0300

    update ar 28/10

--- rtl/MII_generator.sv
+++ rtl/MII_generator.sv
@@ -1,60 +1,94 @@
+`timescale 1ns/100ps
+
 module EthernetFrameGenerator 
 #(
-    parameter int IDLE_CYCLES = 12          ,
-    parameter int PREAMBLE_CYCLES = 7       ,
-    parameter int SFD_CYCLES = 1            ,
-    parameter int DATA_CYCLES = 46          ,// Example data length
-    /*+++++ CODES +++++*/
-    parameter [7:0] IDLE_CODE = 8'h00       ,
+    /*
+    *---------CYCLES---------
+    */
+    parameter int IDLE_CYCLES = 12          ,   //! Idle length
+    parameter int PREAMBLE_CYCLES = 7       ,   //! Preamble length
+    parameter int SFD_CYCLES = 1            ,   //! SFD length
+    parameter int DST_ADDR_CYCLES = 6       ,   
+    parameter int SRC_ADDR_CYCLES = 6       ,
+    parameter int LEN_TYP_CYCLES = 2        ,
+    parameter int FCS_CYCLES = 4            ,
+    parameter int DATA_CYCLES = 46          ,   //! Data length
+    /*
+    *---------CODES---------
+    */
+    parameter [7:0] IDLE_CODE = 8'h07       ,
+    parameter [7:0] START_CODE = 8'hFB      ,   //! No se usa
     parameter [7:0] PREAMBLE_CODE = 8'h55   ,
     parameter [7:0] SFD_CODE = 8'hD5        ,
-    parameter [7:0] EOF_CODE = 8'h00        
+    parameter [7:0] DST_ADDR_CODE = 8'h01   ,
+    parameter [7:0] SRC_ADDR_CODE = 8'h02   ,
+    parameter [7:0] LEN_TYP_CODE = 8'h03    ,
+    parameter [7:0] FCS_CODE = 8'h04        ,
+    parameter [7:0] TERMINATE_CODE = 8'hFD
 )
 (
-    input  logic        clk                 ,   //! Clock input
-    input  logic        i_rst               ,   //! Asynchronous reset
-    input  logic [7:0]  i_interrupt         ,   //! Interrupt the frame into different scenarios
-    output logic [7:0]  o_tx_data           ,   //! Transmitted data (8 bits per cycle)
-    output logic [7:0]  o_tx_ctrl           ,   //! Transmit control signal (indicates valid data)
-    output logic        o_tx_clk            ,   //! Transmit clock
-    input  logic        i_start                 //! Signal to start frame transmission
+    input  logic        clk            ,   //! Clock input
+    input  logic        i_rst          ,   //! Asynchronous reset
+    input  logic        i_start        ,   //! Signal to start frame transmission
+    input  logic [7:0]  i_interrupt    ,   //! Interrupt the frame into different scenarios
+    output logic [7:0]  o_tx_data      ,   //! Transmitted data (8 bits per cycle)
+    output logic [7:0]  o_tx_ctrl          //! Transmit control signal (indicates valid data)
 );
 
     // Parameters for frame sections
-
     localparam int FRAME_SIZE = IDLE_CYCLES + PREAMBLE_CYCLES + SFD_CYCLES + DATA_CYCLES;
-    localparam [7:0] DATA_CHAR_PATTERN = 8'hAA, CTRL_CHAR_PATTERN = 8'h55;
+    localparam [7:0] 
+                    DATA_CHAR_PATTERN = 8'hAA,
+                    CTRL_CHAR_PATTERN = 8'h55;
 
     localparam [3:0]
-                    IDLE        = 0000,
-                    PREAMBLE    = 0001,
-                    SFD         = 0010,
-                    DATA        = 0011,
-                    EOF         = 0100;
+                    IDLE        = 0,
+                    PREAMBLE    = 1,
+                    SFD         = 2,
+                    DST_ADDR    = 3,
+                    SRC_ADDR    = 4,
+                    LEN_TYP     = 5,
+                    DATA        = 6,
+                    FCS         = 7,
+                    EOF         = 8;
 
     localparam [7:0] 
                     STOP_TX     = 8'h01,
                     STOP_DATA   = 8'h02;
-
+    /*
     // Frame content
     logic [7:0] frame [0:FRAME_SIZE-1]          ;
     logic [7:0] random_data [0:DATA_CYCLES-1]   ;
-    //logic [3:0] state, next_state
+
     // Internal signals
     logic [$clog2(FRAME_SIZE)-1:0] frame_index  ;
     logic                          transmitting ;
-    logic [3:0] state, next_state;
-    logic [7:0] tx_data, next_tx_data           ;
-    logic [6:0] counter, next_counter           ;
-    logic [7:0] ctrl_out, next_ctrl_out         ;
+    */
+    // State
+    logic [3:0] state;
+    logic [3:0] next_state;
+
+    // TXD Characters 
+    logic [7:0] tx_data;
+    logic [7:0] next_tx_data;
+
+    // Counter
+    logic [6:0] counter;
+    logic [6:0] next_counter;
+
+    // TXC
+    logic [7:0] tx_ctrl;
+    logic [7:0] next_tx_ctrl;
+
+    // Random
     int random_num;
+
     // Initialize frame content
-
-
     always_comb begin
         next_counter = counter                                                      ;
         next_state = state                                                          ;
         next_tx_data = tx_data                                                      ;
+
         case (state) 
             IDLE: begin
                 if(!i_start) begin
@@ -68,7 +102,7 @@ module EthernetFrameGenerator
 
             PREAMBLE: begin
                 //frame[0] = PREAMBLE_CODE;
-                if(counter < PREAMBLE_CYCLES) begin
+                if(counter < (PREAMBLE_CYCLES-1)) begin
                     next_tx_data = PREAMBLE_CODE                                    ;
                     next_counter = counter + 1                                      ;
                     next_state   = PREAMBLE                                         ;
@@ -80,46 +114,74 @@ module EthernetFrameGenerator
 
             SFD: begin
                 //frame[0] = SFD_CODE;
-                if(counter < SFD_CYCLES) begin
-                    next_tx_data = SFD_CODE                                         ;
+                next_tx_data = SFD_CODE                                             ;
+                next_state = DST_ADDR                                               ;
+                next_counter = 0                                                    ;
+            end
+
+            DST_ADDR: begin
+                if(counter < (DST_ADDR_CYCLES-1)) begin
+                    next_tx_data = DST_ADDR_CODE                                    ;
                     next_counter = counter + 1                                      ;
-                    next_state   = SFD                                              ;
-                end else begin                                  
-                    next_state = DATA                                               ;
+                    next_state   = DST_ADDR                                         ;
+                end else begin
+                    next_state   = SRC_ADDR                                         ;
+                    next_counter = 0                                                ;
+                end
+            end
+
+            SRC_ADDR: begin
+                if(counter < (SRC_ADDR_CYCLES-1)) begin
+                    next_tx_data = SRC_ADDR_CODE                                    ;
+                    next_counter = counter + 1                                      ;
+                    next_state   = SRC_ADDR                                         ;
+                end else begin
+                    next_state   = LEN_TYP                                          ;
+                    next_counter = 0                                                ;
+                end
+            end
+
+            LEN_TYP: begin
+                if(counter < (LEN_TYP_CYCLES-1)) begin
+                    next_tx_data = LEN_TYP_CODE                                     ;
+                    next_counter = counter + 1                                      ;
+                    next_state   = LEN_TYP                                          ;
+                end else begin
+                    next_state   = DATA                                             ;
                     next_counter = 0                                                ;
                 end
             end
 
             DATA: begin
-                
-                 
-                if(counter < DATA_CYCLES) begin
+                if(counter < (DATA_CYCLES-1)) begin
                     if(i_interrupt == STOP_DATA) begin             
-                        next_tx_data = 8'h00                                        ;       
-                    end else begin      
-                        random_num = $urandom_range(0, 99)                          ;
-                        if (random_num < DATA_CHAR_PROBABILITY) begin       
-                            // Data character       
-                            next_tx_data = DATA_CHAR_PATTERN                        ;
-                            next_ctrl_out = {1'b0, ctrl_out[7:1]}                   ;
-                        end else begin      
-                            // Control character        
-                            next_tx_data = CTRL_CHAR_PATTERN                        ;
-                            next_ctrl_out = {1'b0, ctrl_out[7:1]}                   ;
-                        end     
-                    end     
+                        next_tx_data = 8'h00                                        ;    
+                    end else begin
+                        next_tx_data = DATA_CHAR_PATTERN                            ;
+                    end
                     next_counter = counter + 1                                      ;
                     next_state   = DATA                                             ;
                 end else begin
-                    next_state = EOF                                                ;
+                    next_state = FCS                                                ;
+                    next_counter = 0                                                ;
+                end
+            end
+
+            FCS: begin
+                if(counter < (FCS_CYCLES-1)) begin
+                    next_tx_data = FCS_CODE                                         ;
+                    next_counter = counter + 1                                      ;
+                    next_state   = FCS                                              ;
+                end else begin
+                    next_state   = EOF                                              ;
                     next_counter = 0                                                ;
                 end
             end
 
             EOF: begin
                 //frame[0] = EOF_CODE;
-                    next_tx_data = EOF_CODE                                         ;
-                    next_state = PREAMBLE                                           ;
+                    next_tx_data = TERMINATE_CODE                                   ;
+                    next_state = IDLE                                               ;
                     next_counter = 0                                                ;
             end
 
@@ -129,26 +191,17 @@ module EthernetFrameGenerator
         endcase
     end
 
-    // Clock generation (simplified for example)
-    always_ff @(posedge clk or posedge i_rst) begin
-        if (i_rst) begin
-            tx_clk <= 1'b0                                                          ;
-        end else begin
-            tx_clk <= ~tx_clk                                                       ;
-        end
-    end
-
     // Control de transmisión de frames
-    always_ff @(posedge tx_clk or posedge reset) begin
+    always_ff @(posedge clk or posedge i_rst) begin
         if (i_rst) begin 
             tx_data     <= 8'd0                                                     ;
-            ctrl_reg    <= 8'd0                                                     ;
+            tx_ctrl    <= 8'd0                                                     ;
             counter     <= 0                                                        ;
             state       <= IDLE                                                     ;
         end
         else begin
             tx_data <= next_tx_data                                                 ;
-            ctrl_out <= next_ctrl_out                                               ;
+            tx_ctrl <= next_tx_ctrl                                               ;
             counter <= next_counter                                                 ;
             state <= next_state                                                     ;
         end
@@ -156,10 +209,9 @@ module EthernetFrameGenerator
     end
     
     // Asignar los datos transmitidos y la señal de control
-    assign o_tx_data = tx_data ;
-    assign o_tx_ctrl = ctrl_out;
+    assign o_tx_data = tx_data;
+    assign o_tx_ctrl = tx_ctrl;
     //assign tx_data = (transmitting && frame_index < FRAME_SIZE) ? frame[frame_index] : 8'd0;
     //assign tx_ctrl = (transmitting && frame_index < FRAME_SIZE) ? 1'b1 : 1'b0; // Control signal indicating valid data
     
 endmodule
-
--- tb/tb_MII_generator.sv
+++ tb/tb_MII_generator.sv
@@ -1,57 +1,103 @@
-// Code your testbench here
-// or browse Examples
-module tb_EthernetFrameGenerator;
-// Clock and reset signals
-logic clk;
-logic reset;
-logic start;
+`timescale 1ns/100ps
 
-// Outputs from the EthernetFrameGenerator
-logic [7:0] tx_data;
-logic       tx_ctrl;
-logic       tx_clk;
+module EthernetFrameGenerator_tb;
+    /*
+    *---------CYCLES---------
+    */
+    parameter int IDLE_CYCLES = 12          ;   //! Idle length
+    parameter int PREAMBLE_CYCLES = 7       ;   //! Preamble length
+    parameter int SFD_CYCLES = 1            ;   //! SFD length
+    parameter int DST_ADDR_CYCLES = 6       ;   
+    parameter int SRC_ADDR_CYCLES = 6       ;
+    parameter int LEN_TYP_CYCLES = 2        ;
+    parameter int DATA_CYCLES = 46          ;   //! Data length
+    parameter int FCS_CYCLES = 4            ;
+    /*
+    *---------CODES---------
+    */
+    parameter [7:0] IDLE_CODE = 8'h07       ;
+    parameter [7:0] START_CODE = 8'hFB      ;
+    parameter [7:0] PREAMBLE_CODE = 8'h55   ;
+    parameter [7:0] SFD_CODE = 8'hD5        ;
+    parameter [7:0] DST_ADDR_CODE = 8'h01   ;
+    parameter [7:0] SRC_ADDR_CODE = 8'h02   ;
+    parameter [7:0] LEN_TYP_CODE = 8'h03    ;
+    parameter [7:0] FCS_CODE = 8'h04        ;
+    parameter [7:0] TERMINATE_CODE = 8'hFD  ;        
 
-// Instantiate the EthernetFrameGenerator
-EthernetFrameGenerator 
-#(
-    .IDLE_CYCLES(12),
-    .PREAMBLE_CYCLES(7),
-    .SFD_CYCLES(1),
-    .DATA_CYCLES(46),
-    .IDLE_CODE(8'h00),
-    .PREAMBLE_CODE(8'h55),
-    .SFD_CODE(8'hD5),
-    .EOF_CODE(8'h00)
-  
-)uut (
-    .clk(clk),
-    .reset(reset),
-    .tx_data(tx_data),
-    .tx_ctrl(tx_ctrl),
-    .tx_clk(tx_clk),
-    .start(start)
-);
+    // Inputs
+    logic       clk;          //! Clock input
+    logic       i_rst;        //! Asynchronous reset
+    logic [7:0] i_interrupt;  //! Interrupt the frame into different scenarios
+    logic       i_start;      //! Signal to start frame transmission
 
-// Clock generation
-initial begin
-    clk = 0;
-    forever #5 clk = ~clk; // 100 MHz clock
-end
+    // Outputs from the EthernetFrameGenerator
+    logic [7:0] o_tx_data;  //! Transmitted data (8 bits per cycle)
+    logic [7:0] o_tx_ctrl;  //! Transmit control signal (indicates valid data)
 
-// Test sequence
-initial begin
-    reset = 1;
-    start = 0;
-    #20 reset = 0;
-    #20 start = 1;
-    #200 start = 0;
-    #1000 ;
-    $display(Out: %h, tx_data);
-    $finish;
-end
+    // Instantiate the EthernetFrameGenerator
+    EthernetFrameGenerator 
+    #(
+        .IDLE_CYCLES(IDLE_CYCLES),
+        .PREAMBLE_CYCLES(PREAMBLE_CYCLES),
+        .SFD_CYCLES(SFD_CYCLES),
+        .DST_ADDR_CYCLES(DST_ADDR_CYCLES),
+        .SRC_ADDR_CYCLES(SRC_ADDR_CYCLES),
+        .LEN_TYP_CYCLES(LEN_TYP_CYCLES),
+        .DATA_CYCLES(DATA_CYCLES),
+        .FCS_CYCLES(FCS_CYCLES),
+        .IDLE_CODE(IDLE_CODE),
+        .START_CODE(START_CODE),
+        .PREAMBLE_CODE(PREAMBLE_CODE),
+        .SFD_CODE(SFD_CODE),
+        .DST_ADDR_CODE(DST_ADDR_CODE),
+        .SRC_ADDR_CODE(SRC_ADDR_CODE),
+        .LEN_TYP_CODE(LEN_TYP_CODE),
+        .FCS_CODE(FCS_CODE),
+        .TERMINATE_CODE(TERMINATE_CODE)
+    
+    )dut (
+        .clk(clk),
+        .i_rst(i_rst),
+        .i_start(i_start),
+        .i_interrupt(i_interrupt),
+        .o_tx_data(o_tx_data),
+        .o_tx_ctrl(o_tx_ctrl)
+    );
 
-// Monitor outputs
-initial begin
-    $monitor("Time: %0t, tx_data: %h, tx_ctrl: %b, tx_clk: %b", $time, tx_data, tx_ctrl, tx_clk);
-end
+    // Clock generation
+    initial begin
+        clk = 0;
+        forever #5 clk = ~clk; // 100 MHz clock
+    end
+
+    // Test sequence
+    initial begin
+        // Generate .vcd
+        $dumpfile("dump.vcd");
+        $dumpvars;
+
+        // Monitor outputs
+        $display("Out: %h", o_tx_data);
+        $monitor("Time: %0t, o_tx_data: %h, o_tx_ctrl: %b",
+                 $time, o_tx_data, o_tx_ctrl);
+
+        i_rst = 1;
+        i_start = 0;
+        i_interrupt = 0;
+
+        #200;
+        @(posedge clk);
+        i_rst = 0;
+
+        #20;
+        @(posedge clk);
+        i_start = 1;
+        @(posedge clk);
+        i_start = 0;
+
+        #2000;
+        $finish;
+    end
+
 endmodule
