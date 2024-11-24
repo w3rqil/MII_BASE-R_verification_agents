34dc3257ff4b64c42c30cb51d9236818b67d8c37
Rubén Zúñiga <ruben.zuniga@mi.unc.edu.ar>
2024-11-24 14:14:39 -0300

    fixes

--- rtl/BASER_257b_checker.sv
+++ rtl/BASER_257b_checker.sv
@@ -50,20 +50,14 @@ module BASER_257b_checker
 
     // Flag of first 64b ctrl block received in the 257b block
     logic        first_ctrl_block_flag;
-
     // Flag of invalid 64b block detected
     logic        inv_block_flag;
-    
-    /* If the module detects a first 64b ctrl block, offset_first_ctrl_frame = 0
-       If not, offset_first_ctrl_frame = 4 */
-    int          offset_first_ctrl_frame;
 
     always @(*) begin
         // Increase total block counter every clock positive edge
         next_block_count = block_count + 'd1;
         first_ctrl_block_flag = 1'b0;
         inv_block_flag = 1'b0;
-        offset_first_ctrl_frame = 4;
 
         // Detected all data blocks
         if(i_rx_coded[0]) begin
@@ -71,8 +65,8 @@ module BASER_257b_checker
             next_data_count = data_count + 'd1;
             next_ctrl_count = ctrl_count;
 
-            // The block has to be all Data characters
-            if(i_rx_coded[(offset_first_ctrl_frame + 1) +: TC_DATA_WIDTH] != {32{DATA_CHAR_PATTERN}}) begin
+            // Valid block verification logic
+            if(i_rx_coded[1 +: TC_DATA_WIDTH] != {32{DATA_CHAR_PATTERN}}) begin
                 inv_block_flag = 1'b1;
             end
         end
@@ -88,12 +82,7 @@ module BASER_257b_checker
 
                 // Data frame
                 if(i_rx_coded[i+1]) begin
-                    
-                        // The frame has to be all Data characters
-                    if(i_rx_coded[(offset_first_ctrl_frame + 1) + DATA_WIDTH * i +: DATA_WIDTH] != {8{DATA_CHAR_PATTERN}}) begin
-                        inv_block_flag = 1'b1;
-                    end
-                    /*
+
                     // There is no ctrl frame found yet
                     if(first_ctrl_block_flag == 1'b0) begin
 
@@ -111,25 +100,15 @@ module BASER_257b_checker
                             inv_block_flag = 1'b1;
                         end
                     end
-                    */
                 end
 
                 // Ctrl frame
                 else begin
 
-                    // The frame has to be all Ctrl characters
-                    if(i_rx_coded[(offset_first_ctrl_frame + 1) + DATA_WIDTH * i +: DATA_WIDTH] != {8{CTRL_CHAR_PATTERN}}) begin
-                        inv_block_flag = 1'b1;
-                    end
-
-                    // There is a ctrl frame found
-                    offset_first_ctrl_frame = 0;
-
                     // It is the first ctrl frame found
-                    if(offset_first_ctrl_frame == 4) begin
-                        offset_first_ctrl_frame = 0;
+                    if(first_ctrl_block_flag == 1'b0) begin
+                        first_ctrl_block_flag = 1'b1;
 
-                        /*
                         // Ctrl frame formats
                         case (i_rx_coded[5 + DATA_WIDTH * i])
 
@@ -230,10 +209,8 @@ module BASER_257b_checker
                                 inv_block_flag = 1'b1;
                             end
                         endcase
-                        */
                     end
 
-                    /*
                     // It is not the first ctrl frame found
                     else begin
                         
@@ -242,7 +219,6 @@ module BASER_257b_checker
                             inv_block_flag = 1'b1;
                         end
                     end
-                    */
                 end
             end
         end
--- tb/tb_BASER_257b_checker.sv
+++ tb/tb_BASER_257b_checker.sv
@@ -11,8 +11,8 @@ module tb_BASER_257b_checker;
     /*
     *------BLOCK TYPE-------
     */
-    parameter       DATA_CHAR_PATTERN   = 8'hAA                     ;
-    parameter       CTRL_CHAR_PATTERN   = 8'h55                     ;
+    parameter       DATA_CHAR_PATTERN   = 8'h00                     ;
+    parameter       CTRL_CHAR_PATTERN   = 8'hFF                     ;
 
     /*
     *---------INPUTS----------
@@ -40,7 +40,9 @@ module tb_BASER_257b_checker;
                  
         i_rst       = 1;
         clk         = 0;
-        i_rx_coded  = {{TC_DATA_WIDTH / 8{DATA_CHAR_PATTERN}}, 1'b1};
+
+        // Bloque valido: D3 D2 D1 D0
+        i_rx_coded  = {{TC_DATA_WIDTH / 8 {DATA_CHAR_PATTERN}}, 1'b1};
 
         #200;
         @(posedge clk);
@@ -48,23 +50,39 @@ module tb_BASER_257b_checker;
 
         #200;
         @(posedge clk);
-        i_rx_coded[0] = 1'b0;
-        i_rx_coded[4 : 1] = 4'b1110;
-        i_rx_coded[5 +: 60] = {8{CTRL_CHAR_PATTERN}};
-        i_rx_coded[TC_WIDTH - 1: 65] = {24{DATA_CHAR_PATTERN}};
+        // Bloque valido: D3 D2 D1 C0
+        i_rx_coded[0]                  = 1'b0;
+        i_rx_coded[ 4  :    1] = 4'b1110;
+        i_rx_coded[ 5 +:   60] = {  8{CTRL_CHAR_PATTERN}};
+        i_rx_coded[65 +: 3*64] = {3*8{DATA_CHAR_PATTERN}};
 
-        #100;
+        #200;
         @(posedge clk);
-        // i_tx_coded[0] = 0;
+        // Bloque valido: C3 D2 C1 D0
+        i_rx_coded[  4  :  1] = 4'b0101;
+        i_rx_coded[  5 +: 64] = {8{DATA_CHAR_PATTERN}};
+        i_rx_coded[ 69 +: 60] = {8{CTRL_CHAR_PATTERN}};
+        i_rx_coded[129 +: 64] = {8{DATA_CHAR_PATTERN}};
+        i_rx_coded[193 +: 64] = {8{CTRL_CHAR_PATTERN}};
+        
+        #200;
+        @(posedge clk);
+        // Bloque invalido: D3 D2 D1 D0 pero con el header en 0 y los sig 4 bits en 1111
+        i_rx_coded[4 : 1] = 4'b1111;
+        i_rx_coded[  5 +: 3*64] = {3*8{DATA_CHAR_PATTERN}};
+        i_rx_coded[197 +:   60] = {  8{DATA_CHAR_PATTERN}};
 
-        // @(posedge clk);
-        // i_tx_coded[0] = 1;
+        #200;
+        @(posedge clk);
+        // Bloque invalido: D3 D2 C1 D0 pero el C1 tiene 64 bits
+        i_rx_coded[4 : 1] = 4'b1101;
+        i_rx_coded[  5 +: 64] = {8{DATA_CHAR_PATTERN}};
+        i_rx_coded[ 69 +: 64] = {8{CTRL_CHAR_PATTERN}};
+        i_rx_coded[133 +: 64] = {8{DATA_CHAR_PATTERN}};
+        i_rx_coded[197 +: 60] = {8{DATA_CHAR_PATTERN}};
         
-        // @(posedge clk);
-        // i_tx_coded[0] = 0;
-        
-        // @(posedge clk);
-        // i_tx_coded[0] = 1;
+        #200;
+        @(posedge clk);
 
         // Display outputs
         $display("Simulation finished after %0t ps.", $time);
