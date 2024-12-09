a7db68a7d9b6a396342a292e9a09232bb6c52898
Ruben-Zuniga <rubenzu41@gmail.com>
2024-12-09 11:54:41 -0300

    updatetb_gen_check

--- tb/tb_BASER_gen_check.sv
+++ tb/tb_BASER_gen_check.sv
@@ -5,53 +5,74 @@
 */
 
 `timescale 1ns/100ps
-// `include "Modulos/signalGenerator/ejemplo/PCS_gen.sv"
-// `include "Modulos/signalGenerator/rtl/BASER_257b_checker.sv"
+`include "Modulos/signalGenerator/ejemplo/PCS_gen.sv"
+`include "Modulos/signalGenerator/rtl/BASER_257b_checker.sv"
 
 module tb_BASER_gen_check;
 
-    // Parameters
-    parameter int   DATA_WIDTH          = 64                        ;
-    parameter  HDR_WIDTH            = 2                             ;
-    parameter  FRAME_WIDTH          = DATA_WIDTH + HDR_WIDTH        ;
-    parameter  CONTROL_WIDTH        = 8                             ;
-    parameter  TRANSCODER_BLOCKS    = 4                             ;
-    parameter  TRANSCODER_HDR_WIDTH = 4                             ;
-    parameter  PROB                 = 30                            ;
+    /*
+    *-------------------------WIDTH PARAMETERS---------------------------
+    */
+    parameter   DATA_WIDTH              = 64                            ;
+    parameter   HDR_WIDTH               = 2                             ;
+    parameter   FRAME_WIDTH             = DATA_WIDTH + HDR_WIDTH        ;
+    parameter   TC_DATA_WIDTH           = 4 * DATA_WIDTH                ;   // 256 bits transcoded blocks (without header)
+    parameter   TC_HDR_WIDTH            = 1                             ;   // 1 bit sync header
+    parameter   TC_WIDTH                = TC_DATA_WIDTH + TC_HDR_WIDTH  ;   // 257 bits transcoded blocks
+    parameter   CONTROL_WIDTH           = 8                             ;
+    parameter   TRANSCODER_BLOCKS       = 4                             ;
+    parameter   TRANSCODER_HDR_WIDTH    = 4                             ;
+    parameter   PROB                    = 30                            ;
 
-    parameter int   TC_DATA_WIDTH       = 4 * DATA_WIDTH            ;   // 256 bits transcoded blocks (without header)
-    parameter int   SH_WIDTH            = 1                         ;
-    parameter int   TC_WIDTH            = TC_DATA_WIDTH + SH_WIDTH  ;   // 257 bits transcoded blocks
+    /*
+    *-----------------------BLOCK TYPE PARAMETERS------------------------
+    */
+    parameter   DATA_CHAR_PATTERN       = 8'hAA                         ;   // Data character
+    // For Control characters, the standard specifies:
+    // Idle:    0x00
+    // Error:   0x1E
+    parameter   CTRL_CHAR_PATTERN       = 7'h00                         ;   // 7 BITS Control character
+    parameter   OSET_CHAR_PATTERN       = 4'hF                          ;   // 4 BITS Ordered Set character
 
-    parameter       DATA_CHAR_PATTERN   = 8'hAA                     ;
-    parameter       CTRL_CHAR_PATTERN   = 7'h00                     ;
-    parameter       OSET_CHAR_PATTERN   = 4'hF                      ;
+    /*
+    *---------------------INPUTS-------------------------
+    */
+    logic                             clk               ;   // Clock input
+    logic                             i_rst             ;   // Asynchronous reset
 
-    logic [TC_WIDTH  - 1 : 0] o_tx_coded_f0     ;   /* Output transcoder                      */
+    // Generator
+    logic [DATA_WIDTH        - 1 : 0] i_txd             ;   /* Input data                             */
+    logic [CONTROL_WIDTH     - 1 : 0] i_txc             ;   /* Input control byte                     */
+    logic [TRANSCODER_BLOCKS - 1 : 0] i_data_sel_0      ;   /* Data selector                          */
+    logic [                    1 : 0] i_valid           ;   /* Input to enable frame generation       */    
+    logic                             i_enable          ;   /* Flag to enable frame generation        */
+    logic                             i_random_0        ;   /* Flag to enable random frame generation */
+    logic                             i_tx_test_mode    ;   /* Flag to enable TX test mode            */
+    // Checker
+    logic [TC_WIDTH          - 1 : 0] i_rx_xcoded       ;   // Received data
+
+    /*
+    *---------------------OUTPUTS------------------------
+    */
+    // Generator
+    logic [TC_WIDTH          - 1 : 0] o_tx_coded_f0     ;   /* Output transcoder                      */
     logic [FRAME_WIDTH       - 1 : 0] o_frame_0         ;   /* Output frame 0                         */
     logic [FRAME_WIDTH       - 1 : 0] o_frame_1         ;   /* Output frame 1                         */
     logic [FRAME_WIDTH       - 1 : 0] o_frame_2         ;   /* Output frame 2                         */
     logic [FRAME_WIDTH       - 1 : 0] o_frame_3         ;   /* Output frame 3                         */
-    logic [DATA_WIDTH        - 1 : 0] i_txd             ;   /* Input data                             */
-    logic [CONTROL_WIDTH     - 1 : 0] i_txc             ;   /* Input control byte                     */
-    logic [TRANSCODER_BLOCKS - 1 : 0] i_data_sel_0      ;   /* Data selector                          */
-    logic [1                     : 0] i_valid           ;   /* Input to enable frame generation       */    
-    logic                             i_enable          ;   /* Flag to enable frame generation        */
-    logic                             i_random_0        ;   /* Flag to enable random frame generation */
-    logic                             i_tx_test_mode    ;   /* Flag to enable TX test mode            */
-
-    logic                    clk                ;   // Clock input
-    logic                    i_rst              ;   // Asynchronous reset
-    logic    [TC_WIDTH-1:0]  i_rx_xcoded         ;   // Received data
-
-    logic    [FRAME_WIDTH-1:0]  o_rx_coded_0       ;   // 1st 64b block
-    logic    [FRAME_WIDTH-1:0]  o_rx_coded_1       ;   // 2nd 64b block
-    logic    [FRAME_WIDTH-1:0]  o_rx_coded_2       ;   // 3rd 64b block
-    logic    [FRAME_WIDTH-1:0]  o_rx_coded_3       ;   // 4th 64b block
-    logic    [31:0]             o_block_count      ;   // Total number of 257b blocks received
-    logic    [31:0]             o_data_count       ;   // Total number of 257b blocks with all 64b data block received
-    logic    [31:0]             o_ctrl_count       ;   // Total number of 257b blocks with at least one 64b control block received
-    logic    [31:0]             o_inv_block_count  ;   // Total number of invalid blocks
+    // Checker
+    logic [FRAME_WIDTH       - 1 : 0] o_rx_coded_0              ;   // 1st 64b block
+    logic [FRAME_WIDTH       - 1 : 0] o_rx_coded_1              ;   // 2nd 64b block
+    logic [FRAME_WIDTH       - 1 : 0] o_rx_coded_2              ;   // 3rd 64b block
+    logic [FRAME_WIDTH       - 1 : 0] o_rx_coded_3              ;   // 4th 64b block
+    logic [                   31 : 0] o_block_count             ;   // Total number of 257b blocks received
+    logic [                   31 : 0] o_data_count              ;   // Total number of 257b blocks with all 64b data block received
+    logic [                   31 : 0] o_ctrl_count              ;   // Total number of 257b blocks with at least one 64b control block received
+    logic [                   31 : 0] o_inv_block_count         ;   // Total number of invalid blocks
+    logic [                   31 : 0] o_inv_ctrl_block_count    ;   // Total number of 257b blocks with invalid 64b ctrl blocks
+    logic [                   31 : 0] o_inv_data_block_count    ;   // Total number of 257b blocks with invalid 64b data blocks
+    logic [                   31 : 0] o_inv_sequence_count      ;   // Total number of 257b blocks with with invalid 64b format
+    logic [                   31 : 0] o_inv_sh_count            ;   // Total number of 257b blocks with invalid sync header
 
     // Clock generation
     always #5 clk = ~clk; // 100 MHz clock
@@ -59,8 +80,8 @@ module tb_BASER_gen_check;
     assign i_rx_xcoded = o_tx_coded_f0;
 
     initial begin
-        // $dumpfile("Modulos/signalGenerator/tb/tb_BASER_gen_check.vcd");
-        // $dumpvars();
+        $dumpfile("Modulos/signalGenerator/tb/tb_BASER_gen_check.vcd");
+        $dumpvars();
         clk             = 'b0       ;
         i_rst           = 'b1       ;
         i_data_sel_0    = 'b0000    ;
@@ -73,18 +94,24 @@ module tb_BASER_gen_check;
         #100                        ;
 
         // Set the enable and desactive the reset
-        i_rst         = 1'b0      ;
+        i_rst           = 1'b0      ;
         i_enable        = 1'b1      ;
-        i_valid         = 2'b11    ;
+        i_valid         = 2'b11     ;
 
-        // Set the data sel 0 to data
+        // Set the data sel 0
+        #300;
         i_data_sel_0    = 4'b0001   ;
-        #100                        ;
-        i_data_sel_0 = 4'b0010;
-        #100;
-        // Set the data sel 0 to ctrl
-        i_data_sel_0    = 4'b0000   ;
-        #100                        ;
+        #300                        ;
+        i_data_sel_0    = 4'b0010   ;
+        #300;
+        i_data_sel_0    = 4'b0011   ;
+        #300                        ;
+        i_data_sel_0    = 4'b0100   ;
+        #300                        ;
+        i_data_sel_0    = 4'b1000   ;
+        #300                        ;
+        i_data_sel_0    = 4'b1111   ;
+        #300                        ;
 
         // Change the inputs
         i_enable        = 1'b0;
@@ -135,7 +162,7 @@ module tb_BASER_gen_check;
         .FRAME_WIDTH          (FRAME_WIDTH          )   ,
         .CONTROL_WIDTH        (CONTROL_WIDTH        )   ,
         .TRANSCODER_BLOCKS    (TRANSCODER_BLOCKS    )   ,
-        .TC_WIDTH             (TC_WIDTH             )   ,
+        .TRANSCODER_WIDTH     (TC_WIDTH             )   ,
         .TRANSCODER_HDR_WIDTH (TRANSCODER_HDR_WIDTH )   ,
         .PROB                 (PROB                 )
     )
